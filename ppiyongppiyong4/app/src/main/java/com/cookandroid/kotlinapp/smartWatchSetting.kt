1c2ed2b2abe0d566232a7f7a11e5569dee362b27
kys17326 <48546591+kys17326@users.noreply.github.com>
2019-07-04 14:56:58 +0900

    Update smartWatchSetting.kt

--- ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/smartWatchSetting.kt
+++ ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/smartWatchSetting.kt
@@ -1,13 +1,24 @@
 package com.cookandroid.kotlinapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 
-class smartWatchSetting: Common(){
+
+
+
+
+@SuppressLint("Regiesterd")
+open class smartWatchSetting: Common(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        
+
+
+
+
     }
 }
