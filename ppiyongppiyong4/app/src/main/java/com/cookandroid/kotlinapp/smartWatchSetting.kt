9abb91ca5213771e27fe2f9d74c83a8e47ca50b9
KYS <kys17326@naver.com>
2019-07-14 21:48:39 +0900

    Update smartWatchSetting.kt

--- ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/smartWatchSetting.kt
+++ ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/smartWatchSetting.kt
@@ -1,14 +1,82 @@
 package com.cookandroid.kotlinapp
 
-import android.annotation.SuppressLint
-import android.os.Bundle
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.bluetooth_scan.*
+import org.jetbrains.anko.toast
 
-class smartWatchSetting: Common() {
+class smartWatchSetting: PreferenceActivity() {
+
+    private var mBluetoothAdapter:BluetoothAdapter? = null
+    private lateinit var mPairedDevice: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.bluetooth_scan)
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(mBluetoothAdapter == null){
+            toast("블루투스를 지원 하지 않는 디바이스 입니다.")
+            return
+        }
+        if(!mBluetoothAdapter!!.isEnabled){
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+
+        select_device_refresh.setOnClickListener {
+            mPairedDevicedDeviceList()
+        }
+
+    }
+    private fun mPairedDevicedDeviceList() {
+        mPairedDevice = mBluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+
+        if (!mPairedDevice.isEmpty()){
+            for (device: BluetoothDevice in mPairedDevice){
+                list.add(device)
+                Log.i("device", ""+device)
+            }
+        }else{
+            toast("연결된 블루투스 장치가 없습니다.")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener{_,_,position,_ ->
+            val device:BluetoothDevice = list[position]
+            val address:String = device.address
+            val intent = Intent(this, ControlActivity::class.java)
+            intent.putExtra(EXTRA_ADDRESS,address)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            if(resultCode == Activity.RESULT_OK){
+                if(mBluetoothAdapter!!.isEnabled){
+                    toast("블루투스가 사용 설정되었습니다.")
+                }else {
+                    toast("블루투스가 사용이 중지 되었습니다.")
+                }
+            }else if(resultCode == Activity.RESULT_CANCELED){
+                toast("블루투스 사용이 취소되었습니다.")
+            }
+        }
     }
 }
