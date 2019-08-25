be0aefc4a8515f86165ad8b571252f1ec9a8cb4d
ImSeongMuk <49136613+ImSeongMuk@users.noreply.github.com>
2019-08-25 15:49:47 +0900

    비밀번호 수정 및 비밀번호 찾기 완료 (비밀번호 변경할때 정규 표현식 설정해야함)

--- ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/PW_change.kt
+++ ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/PW_change.kt
@@ -1,11 +1,19 @@
 package com.cookandroid.kotlinapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.header.*
+import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.pw_change.*
+import org.json.JSONObject
 
 class PW_change : Common() {
 
@@ -14,5 +22,56 @@ class PW_change : Common() {
         setContentView(R.layout.pw_change)
         btnHeaderSetting.visibility = View.GONE
         txtHeaderTitle.text="비밀번호 변경";
+
+        txtNewPw
+        txtNowPw
+
+        btnSubmit.setOnClickListener {
+            val settings: SharedPreferences = getSharedPreferences("userNumber", MODE_PRIVATE)
+            val url = "http://61.84.24.251:49090/siren/passwordUpdate"
+            val params = HashMap<String, String>()
+            //현재 비밀번호가 true일때 실행
+            params["email"] = settings.getString("email",null)
+            params["password"] = txtNowPw.text.toString()
+            params["passwordNew"] = txtNewPw.text.toString()
+            val jsonObject = JSONObject(params)
+            val request = JsonObjectRequest(Request.Method.POST,url,jsonObject,
+                Response.Listener { response ->
+                    // Process the json
+                    try {
+                        println(" Response: $response")
+                        if (response.getString("result").equals("T")){
+                            //로그인 성고하면 사용자의 userNum을 저장한다
+                            println("비밀번호 변경완료")
+                            Toast.makeText(this, "비밀변호가 변경 되었습니다.", Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this, "현재 비밀번호가 잘못되었습니다.",Toast.LENGTH_SHORT).show()
+                        }
+                        //println(response.getString("result"))
+                        //txtId.text = "Response: $response"
+
+                    }catch (e:Exception){
+                        println(" Exception: $e")
+                        //txtPw.text = "Exception: $e"
+                    }
+
+                }, Response.ErrorListener{
+                    // Error in request
+                    println(" Volley error: $it")
+                    //txtId.text = "Volley error: $it"
+                }
+            )
+
+            request.retryPolicy = DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
+                // 0 means no retry
+                0, // DefaultRetryPolicy.DEFAULT_MAX_RETRIES = 2
+                1f // DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            )
+            // Add the volley post request to the request queue
+            VolleySingleton.getInstance(this).addToRequestQueue(request)
+            startActivity(Intent(this,Setting::class.java))
+        }
+
     }
 }
